598932fceb56b22adead4844a6c2039e2113382d
luckyadogun <meetluckyadogun@gmail>
2021-05-12 13:42:56 +0100

    tested operands

--- src/section_one/smart_calculator.py
+++ src/section_one/smart_calculator.py
@@ -2,11 +2,16 @@ class OperatorError(Exception):
     pass
 
 
-class InputError(Exception):
+class OperandError(Exception):
     pass
 
 
 def calculate(
     operand_1: int, operand_2: int, operator: str
 ):
-    pass
+    if not isinstance(operand_1, int) and not isinstance(
+        operand_2, int
+    ):
+        raise OperandError(
+            "<operands> can only be integer values"
+        )
--- tests/section_one/test_calculator.py
+++ tests/section_one/test_calculator.py
@@ -1,6 +1,10 @@
 import pytest
 
-from src.section_one.smart_calculator import calculate
+from src.section_one.smart_calculator import (
+    calculate,
+    OperandError,
+    OperatorError,
+)
 
 
 class TestCalculator:
@@ -11,3 +15,19 @@ class TestCalculator:
         THEN calculate must exist in the global namespace
         """
         assert "calculate" in globals()
+
+    def test_calculator_can_only_accept_integer_operands(
+        self,
+    ):
+        """
+        GIVEN a non-integer operand values
+        WHEN calculate function is called
+        THEN an exception is raised with specific message
+        """
+        with pytest.raises(OperandError) as exc:
+            calculate("a", "b", "add")
+
+        assert (
+            str(exc.value)
+            == "<operands> can only be integer values"
+        )
