996e182756a7ca2f979ffd1d9d834ed29356b8fb
luckyadogun <meetluckyadogun@gmail>
2021-05-12 15:18:48 +0100

    added calculator funcs

--- src/section_one/smart_calculator.py
+++ src/section_one/smart_calculator.py
@@ -1,3 +1,6 @@
+import operator as math_operator
+
+
 class OperatorError(Exception):
     pass
 
@@ -6,12 +9,56 @@ class OperandError(Exception):
     pass
 
 
+ARITHEMETIC_KEYS = {
+    "maths": {
+        "+": math_operator.add,
+        "-": math_operator.sub,
+        "*": math_operator.mul,
+        "/": math_operator.truediv,
+    },
+    "english": {
+        "add": math_operator.add,
+        "subtract": math_operator.sub,
+        "multiply": math_operator.mul,
+        "divide": math_operator.truediv,
+    },
+}
+
+
 def calculate(
-    operand_1: int, operand_2: int, operator: str
+    operand_1: int,
+    operand_2: int,
+    operator: str,
+    math_func=ARITHEMETIC_KEYS,
 ):
+
     if not isinstance(operand_1, int) and not isinstance(
         operand_2, int
     ):
         raise OperandError(
             "<operands> can only be integer values"
+        )
+
+    if not isinstance(operator, str):
+        raise OperatorError(
+            "<operator> can only be in mathematical string representations eg: '+' or 'add'"
+        )
+
+    if operator in math_func["maths"]:
+        return math_func["maths"][operator](
+            operand_1, operand_2
+        )
+    elif operator in math_func["english"]:
+        return math_func["english"][operator](
+            operand_1, operand_2
+        )
+    else:
+        raise OperatorError(
+            """
+            Invalid operator keys. Try:
+                - '+" or 'add'
+                - '-' or 'subtract'
+                - '/' or 'divide'
+                - '*' or 'multiply'
+            """
         )
--- tests/section_one/test_calculator.py
+++ tests/section_one/test_calculator.py
@@ -31,3 +31,55 @@ class TestCalculator:
             str(exc.value)
             == "<operands> can only be integer values"
         )
+
+    def test_calculator_can_only_accept_math_string_operators(
+        self,
+    ):
+        """
+        GIVEN a non-string or non-math string operator value
+        WHEN calculate function is called
+        THEN an exception is raised with specific message
+        """
+        with pytest.raises(
+            OperatorError
+        ) as exc_invalid_opr:
+            calculate(1, 2, True)
+
+        with pytest.raises(
+            OperatorError
+        ) as exc_invalid_key:
+            calculate(1, 2, "adds")
+
+        assert (
+            str(exc_invalid_opr.value)
+            == "<operator> can only be in mathematical string representations eg: '+' or 'add'"
+        )
+
+        assert (
+            str(exc_invalid_key.value)
+            == """
+            Invalid operator keys. Try:
+                - '+" or 'add'
+                - '-' or 'subtract'
+                - '/' or 'divide'
+                - '*' or 'multiply'
+            """
+        )
+
+    def test_simple_math_funcs_work(self):
+        """
+        GIVEN a correct operator and integer operands
+        WHEN calculate function is called
+        THEN the expected output is returned
+        """
+        assert calculate(2, 2, "+") == 4
+        assert calculate(2, 2, "add") == 4
+
+        assert calculate(2, 8, "multiply") == 16
+        assert calculate(2, 8, "*") == 16
+
+        assert calculate(2, 10, "-") == -8
+        assert calculate(10, 2, "subtract") == 8
+
+        assert calculate(2, 20, "/") == 0.1
+        assert calculate(20, 2, "divide") == 10
